932a8cf64d3d3ed076e3867daf207525d80b427f
Mateo Toledano <mateo.toledano@gmail.com>
2024-06-07 22:05:53 -0300

    Solucion challenge-2

--- challenge-2/backend/entrypoint.sh
+++ challenge-2/backend/entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# wait for Postgres container to be ready
+while ! nc -z -v $SQL_HOST $SQL_PORT; do
+  echo "Waiting for database to be ready..."
+  sleep 1
+done
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
+echo "Database is ready, proceeding with Django commands."
 
 # Uncomment below to flush db e.g. after running tests
 # Just make sure you really mean it 
@@ -17,10 +14,31 @@ fi
 
 # We have base custom user model so need to makemigrations out of box
 
-python manage.py makemigrations core
+python manage.py makemigrations
 python manage.py flush --no-input
 python manage.py migrate
 python manage.py loaddata initial_data.json
+
+# Create superuser
+echo "Creating superuser..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = '$SUPERUSER_USERNAME'
+email = '$SUPERUSER_EMAIL'
+password = '$SUPERUSER_PASSWORD'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser {username} created successfully!")
+else:
+    print(f"Superuser {username} already exists.")
+EOF
+
+echo "Starting Django server..."
+
+
 # python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
 
